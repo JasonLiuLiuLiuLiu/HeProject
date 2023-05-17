01fa4da63c0fb585e8fe71bef49dc20261e37987
JasonLiu <liuzhenyulive@live.com>
2023-05-18 00:13:26 +0800

    0518

--- HeProject_V2/HeProject/202305/P202305.cs
+++ HeProject_V2/HeProject/202305/P202305.cs
@@ -2,6 +2,7 @@
 using NPOI.SS.UserModel;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace HeProject._202305
@@ -9,35 +10,501 @@ namespace HeProject._202305
     public class P202305
     {
         private IWorkbook _workBook;
+        private ISheet _sheet;
+        private int _lastRowIndex;
         private Dictionary<int, bool?[]> _history;
         public P202305(IWorkbook workbook)
         {
             _workBook = workbook;
+            _sheet = _workBook.GetSheetAt(0);
+            _lastRowIndex = _sheet.LastRowNum - 1 - 12;
             _history = CalculateHistory();
         }
         public void Start()
         {
             SetHeader();
+            Display1();
+            Display2();
+            Display3();
+            Display4();
+            Display5();
+            Display6();
+            Display7();
+            Display8();
+            Display9();
+            Display10();
+            Display11();
+            Display12();
         }
 
+        private Dictionary<int, List<KeyValuePair<int, bool?>>> _display1 = new Dictionary<int, List<KeyValuePair<int, bool?>>>();
         public void Display1()
         {
-            foreach(var rowItem in _history)
+            foreach (var rowItem in _history)
             {
-                var rowIndex = rowItem.Key;
-                for (var i= 0; i < 9; i++)
-                {
 
+                var rowIndex = rowItem.Key;
+                if (rowIndex > _lastRowIndex && _history[rowIndex].All(u => u == null))
+                    continue;
+
+                var value = 0;
+                var hitColumns = new List<KeyValuePair<int, bool?>>();
+                rowIndex = GetPreRow(rowIndex);
+                if (_history.ContainsKey(rowIndex))
+                {
+                    for (var i = 0; i < 9; i++)
+                    {
+                        var value1 = PreValue(rowIndex, i);
+                        if (value1.HasValue)
+                        {
+                            var value2 = PreValue(GetPreRow(value1.Value.Key), i);
+                            if (value1.Value.Value == value2?.Value)
+                            {
+                                hitColumns.Add(new KeyValuePair<int, bool?>(i, value1.Value.Value));
+                                value++;
+                            }
+
+                        }
+                    }
+                }
+
+                var cell = _sheet.GetRow(rowItem.Key)?.CreateCell(278);
+                cell?.SetCellValue(value);
+
+                if (value != 0)
+                {
+                    _display1.Add(rowItem.Key, hitColumns);
                 }
             }
         }
 
-        private KeyValuePair<int,bool>? PreValue(int row, int column)
+        public void Display2()
         {
-            if (row == 1)
+            foreach (var rowItem in _history)
+            {
+
+                var rowIndex = rowItem.Key;
+                if (rowIndex > _lastRowIndex && _history[rowIndex].All(u => u == null))
+                    continue;
+
+                var value = 0;
+                if (_history.ContainsKey(rowIndex))
+                {
+                    for (var i = 0; i < 9; i++)
+                    {
+                        if (!_display1.ContainsKey(rowIndex) || !_display1[rowIndex].Select(u => u.Key).Contains(i))
+                            continue;
+
+                        var value1 = _history[rowIndex][i];
+                        if (value1 != null)
+                        {
+                            var value2 = PreValue(GetPreRow(rowIndex), i);
+                            if (value2?.Value == value1)
+                                value++;
+                        }
+                    }
+                }
+
+                var cell = _sheet.GetRow(rowItem.Key)?.CreateCell(279);
+                cell?.SetCellValue(value);
+            }
+        }
+
+        public void Display3()
+        {
+            foreach (var rowItem in _history)
+            {
+
+                var rowIndex = rowItem.Key;
+                if (rowIndex > _lastRowIndex && _history[rowIndex].All(u => u == null))
+                    continue;
+
+                var value = 0;
+                if (_history.ContainsKey(rowIndex))
+                {
+                    for (var i = 0; i < 9; i++)
+                    {
+                        if (!_display1.ContainsKey(rowIndex) || !_display1[rowIndex].Select(u => u.Key).Contains(i))
+                            continue;
+
+                        var value1 = _history[rowIndex][i];
+                        if (value1 != null)
+                        {
+                            var value2 = _display1[rowIndex].FirstOrDefault(u => u.Key == i).Value;
+                            if (value2 != null && value2.Value != value1)
+                                value++;
+                        }
+                    }
+                }
+
+                var cell = _sheet.GetRow(rowItem.Key)?.CreateCell(280);
+                cell?.SetCellValue(value);
+            }
+        }
+
+        private Dictionary<int, List<KeyValuePair<int, bool?>>> _display4 = new Dictionary<int, List<KeyValuePair<int, bool?>>>();
+        public void Display4()
+        {
+            foreach (var rowItem in _history)
+            {
+
+                var rowIndex = rowItem.Key;
+                if (rowIndex > _lastRowIndex && _history[rowIndex].All(u => u == null))
+                    continue;
+
+                var value = 0;
+                var hitColumns = new List<KeyValuePair<int, bool?>>();
+                rowIndex = GetPreRow(rowIndex);
+                if (_history.ContainsKey(rowIndex))
+                {
+                    for (var i = 0; i < 9; i++)
+                    {
+                        var value1 = PreValue(rowIndex, i);
+                        if (value1.HasValue)
+                        {
+                            var value2 = PreValue(GetPreRow(value1.Value.Key), i);
+                            if (value2.HasValue && value1.Value.Value != value2?.Value)
+                            {
+                                hitColumns.Add(new KeyValuePair<int, bool?>(i, value1.Value.Value));
+                                value++;
+                            }
+
+                        }
+                    }
+                }
+
+                var cell = _sheet.GetRow(rowItem.Key)?.CreateCell(281);
+                cell?.SetCellValue(value);
+
+                if (value != 0)
+                {
+                    _display4.Add(rowItem.Key, hitColumns);
+                }
+            }
+        }
+
+        public void Display5()
+        {
+            foreach (var rowItem in _history)
+            {
+
+                var rowIndex = rowItem.Key;
+                if (rowIndex > _lastRowIndex && _history[rowIndex].All(u => u == null))
+                    continue;
+
+                var value = 0;
+                if (_history.ContainsKey(rowIndex))
+                {
+                    for (var i = 0; i < 9; i++)
+                    {
+                        if (!_display4.ContainsKey(rowIndex) || !_display4[rowIndex].Select(u => u.Key).Contains(i))
+                            continue;
+
+                        var value1 = _history[rowIndex][i];
+                        if (value1 != null)
+                        {
+                            var value2 = PreValue(GetPreRow(rowIndex), i);
+                            if (value2?.Value == value1)
+                                value++;
+                        }
+                    }
+                }
+
+                var cell = _sheet.GetRow(rowItem.Key)?.CreateCell(282);
+                cell?.SetCellValue(value);
+            }
+        }
+
+        public void Display6()
+        {
+            foreach (var rowItem in _history)
+            {
+
+                var rowIndex = rowItem.Key;
+                if (rowIndex > _lastRowIndex && _history[rowIndex].All(u => u == null))
+                    continue;
+
+                var value = 0;
+                if (_history.ContainsKey(rowIndex))
+                {
+                    for (var i = 0; i < 9; i++)
+                    {
+                        if (!_display4.ContainsKey(rowIndex) || !_display4[rowIndex].Select(u => u.Key).Contains(i))
+                            continue;
+
+                        var value1 = _history[rowIndex][i];
+                        if (value1 != null)
+                        {
+                            var value2 = _display4[rowIndex].FirstOrDefault(u => u.Key == i).Value;
+                            if (value2 != null && value2.Value != value1)
+                                value++;
+                        }
+                    }
+                }
+
+                var cell = _sheet.GetRow(rowItem.Key)?.CreateCell(283);
+                cell?.SetCellValue(value);
+            }
+        }
+
+        private Dictionary<int, List<KeyValuePair<int, bool?>>> _display7 = new Dictionary<int, List<KeyValuePair<int, bool?>>>();
+        public void Display7()
+        {
+            foreach (var rowItem in _history)
+            {
+
+                var rowIndex = rowItem.Key;
+                if (rowIndex > _lastRowIndex && _history[rowIndex].All(u => u == null))
+                    continue;
+
+                var value = 0;
+                var hitColumns = new List<KeyValuePair<int, bool?>>();
+                rowIndex = GetPreRow(rowIndex);
+                if (_history.ContainsKey(rowIndex))
+                {
+                    for (var i = 0; i < 9; i++)
+                    {
+                        var value1 = PreValue(rowIndex, i);
+                        if (value1.HasValue)
+                        {
+                            var value2 = PreValue(GetPreRow(value1.Value.Key), i);
+                            if (value2 != null)
+                            {
+                                value2 = PreValue(GetPreRow(value2.Value.Key), i);
+                                if (value1.Value.Value == value2?.Value)
+                                {
+                                    hitColumns.Add(new KeyValuePair<int, bool?>(i, value1.Value.Value));
+                                    value++;
+                                }
+                            }
+                        }
+                    }
+                }
+
+                var cell = _sheet.GetRow(rowItem.Key)?.CreateCell(284);
+                cell?.SetCellValue(value);
+
+                if (value != 0)
+                {
+                    _display7.Add(rowItem.Key, hitColumns);
+                }
+            }
+        }
+
+        public void Display8()
+        {
+            foreach (var rowItem in _history)
+            {
+
+                var rowIndex = rowItem.Key;
+                if (rowIndex > _lastRowIndex && _history[rowIndex].All(u => u == null))
+                    continue;
+
+                var value = 0;
+                if (_history.ContainsKey(rowIndex))
+                {
+                    for (var i = 0; i < 9; i++)
+                    {
+                        if (!_display7.ContainsKey(rowIndex) || !_display7[rowIndex].Select(u => u.Key).Contains(i))
+                            continue;
+
+                        var value1 = _history[rowIndex][i];
+                        if (value1 != null)
+                        {
+                            var value2 = PreValue(GetPreRow(rowIndex), i);
+                            if (value2 != null)
+                            {
+                                value2 = PreValue(GetPreRow(value2.Value.Key), i);
+                                if (value1.Value == value2?.Value)
+                                {
+                                    value++;
+                                }
+                            }
+
+                        }
+                    }
+                }
+
+                var cell = _sheet.GetRow(rowItem.Key)?.CreateCell(285);
+                cell?.SetCellValue(value);
+            }
+        }
+
+        public void Display9()
+        {
+            foreach (var rowItem in _history)
+            {
+
+                var rowIndex = rowItem.Key;
+                if (rowIndex > _lastRowIndex && _history[rowIndex].All(u => u == null))
+                    continue;
+
+                var value = 0;
+                if (_history.ContainsKey(rowIndex))
+                {
+                    for (var i = 0; i < 9; i++)
+                    {
+                        if (!_display7.ContainsKey(rowIndex) || !_display7[rowIndex].Select(u => u.Key).Contains(i))
+                            continue;
+
+                        var value1 = _history[rowIndex][i];
+                        if (value1 != null)
+                        {
+                            var value2 = PreValue(GetPreRow(rowIndex), i);
+                            if (value2 != null)
+                            {
+                                value2 = PreValue(GetPreRow(value2.Value.Key), i);
+                                if (value1.Value != value2?.Value)
+                                {
+                                    value++;
+                                }
+                            }
+
+                        }
+                    }
+                }
+
+                var cell = _sheet.GetRow(rowItem.Key)?.CreateCell(286);
+                cell?.SetCellValue(value);
+            }
+        }
+
+        private Dictionary<int, List<KeyValuePair<int, bool?>>> _display10 = new Dictionary<int, List<KeyValuePair<int, bool?>>>();
+        public void Display10()
+        {
+            foreach (var rowItem in _history)
+            {
+
+                var rowIndex = rowItem.Key;
+                if (rowIndex > _lastRowIndex && _history[rowIndex].All(u => u == null))
+                    continue;
+
+                var value = 0;
+                var hitColumns = new List<KeyValuePair<int, bool?>>();
+                rowIndex = GetPreRow(rowIndex);
+                if (_history.ContainsKey(rowIndex))
+                {
+                    for (var i = 0; i < 9; i++)
+                    {
+                        var value1 = PreValue(rowIndex, i);
+                        if (value1.HasValue)
+                        {
+                            var value2 = PreValue(GetPreRow(value1.Value.Key), i);
+                            if (value2 != null)
+                            {
+                                value2 = PreValue(GetPreRow(value2.Value.Key), i);
+                                if (value1.Value.Value != value2?.Value)
+                                {
+                                    hitColumns.Add(new KeyValuePair<int, bool?>(i, value1.Value.Value));
+                                    value++;
+                                }
+                            }
+                        }
+                    }
+                }
+
+                var cell = _sheet.GetRow(rowItem.Key)?.CreateCell(287);
+                cell?.SetCellValue(value);
+
+                if (value != 0)
+                {
+                    _display10.Add(rowItem.Key, hitColumns);
+                }
+            }
+        }
+
+        public void Display11()
+        {
+            foreach (var rowItem in _history)
+            {
+
+                var rowIndex = rowItem.Key;
+                if (rowIndex > _lastRowIndex && _history[rowIndex].All(u => u == null))
+                    continue;
+
+                var value = 0;
+                if (_history.ContainsKey(rowIndex))
+                {
+                    for (var i = 0; i < 9; i++)
+                    {
+                        if (!_display10.ContainsKey(rowIndex) || !_display10[rowIndex].Select(u => u.Key).Contains(i))
+                            continue;
+
+                        var value1 = _history[rowIndex][i];
+                        if (value1 != null)
+                        {
+                            var value2 = PreValue(GetPreRow(rowIndex), i);
+                            if (value2 != null)
+                            {
+                                value2 = PreValue(GetPreRow(value2.Value.Key), i);
+                                if (value1.Value != value2?.Value)
+                                {
+                                    value++;
+                                }
+                            }
+
+                        }
+                    }
+                }
+
+                var cell = _sheet.GetRow(rowItem.Key)?.CreateCell(288);
+                cell?.SetCellValue(value);
+            }
+        }
+
+        public void Display12()
+        {
+            foreach (var rowItem in _history)
+            {
+
+                var rowIndex = rowItem.Key;
+                if (rowIndex > _lastRowIndex && _history[rowIndex].All(u => u == null))
+                    continue;
+
+                var value = 0;
+                if (_history.ContainsKey(rowIndex))
+                {
+                    for (var i = 0; i < 9; i++)
+                    {
+                        if (!_display10.ContainsKey(rowIndex) || !_display10[rowIndex].Select(u => u.Key).Contains(i))
+                            continue;
+
+                        var value1 = _history[rowIndex][i];
+                        if (value1 != null)
+                        {
+                            var value2 = PreValue(GetPreRow(rowIndex), i);
+                            if (value2 != null)
+                            {
+                                value2 = PreValue(GetPreRow(value2.Value.Key), i);
+                                if (value1.Value == value2?.Value)
+                                {
+                                    value++;
+                                }
+                            }
+
+                        }
+                    }
+                }
+
+                var cell = _sheet.GetRow(rowItem.Key)?.CreateCell(289);
+                cell?.SetCellValue(value);
+            }
+        }
+
+        private int GetPreRow(int row)
+        {
+            if (row <= _lastRowIndex)
+                return row - 1;
+
+            return _lastRowIndex;
+        }
+
+        private KeyValuePair<int, bool>? PreValue(int row, int column)
+        {
+            if (row <= 1)
                 return null;
 
-            if(!_history.ContainsKey(row))
+            if (!_history.ContainsKey(row))
                 return null;
 
             if (_history[row][column].HasValue)
@@ -48,13 +515,13 @@ namespace HeProject._202305
 
         public void SetHeader()
         {
-            var sheet = _workBook.GetSheetAt(0);
-            var headerRow = sheet.GetRow(0);
+
+            var headerRow = _sheet.GetRow(0);
             for (int i = 0; i < 21; i++)
             {
                 var cell = headerRow.CreateCell(i + 278);
                 cell.SetCellValue(i + 1);
-                sheet.SetColumnWidth(i + 278, 700);
+                _sheet.SetColumnWidth(i + 278, 700);
             }
         }
 
@@ -78,7 +545,7 @@ namespace HeProject._202305
                     {
                         rowResult[rowIndex] = true;
                     }
-                    else if (cell.CellStyle.FillForegroundColor == 12)
+                    else if (cell.CellStyle.FillForegroundColor == 40)
                     {
                         rowResult[rowIndex] = false;
                     }
